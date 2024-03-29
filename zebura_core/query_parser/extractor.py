137b11036c3c4e674d5e87c9398f0dc74b85d200
harusamei <harusamei@126.com>
2024-03-29 16:07:05 +0800

    FEAT:add extract key words from sql statement

--- zebura_core/query_parser/extractor.py
+++ zebura_core/query_parser/extractor.py
@@ -1,34 +1,45 @@
 import os
 import sys
 import re
-
 sys.path.insert(0, os.getcwd())
 import settings
 import info_loader
 
-
 class Extractor:
+
     def __init__(self):
-        self.pattern = r"SELECT\s+(.*?)\s+FROM\s+(.*?)\s*;"
-        self.slots = ['select','table','condition','limit','distinct','group_by','order_by','as','like','offset']
-        # select, group_by, order_by 为多值用,分隔
-        self.infoDict = {slot:None for slot in self.slots}
-
+        self.loader = info_loader.PatLoader()
+       
+    def create_empty_slots(self):
+        slots = dict.fromkeys(self.loader.sql_pats.keys())
+        return slots
+    
     def extract_info(self,sql):
-        # Regular expression pattern to match SELECT statements
-        pattern = r"FROM\s+(.*?)\s*(WHERE|;)"
+        if sql[-1] != ';':
+            sql += ';'
+        # Regular expression pattern to match sql statements
+        steps = ['select','sql_from', 'where', 'distinct', 'limit', 'offset', 'group_by', 'order_by', 'as', 'like']
+        slots = self.create_empty_slots()
+        for step in steps:
+            pat = self.loader[step]   
+            if not pat:
+               print(f"Error: {step} pattern is not found")
+               continue
+            # Extract the table name and columns using regex
+            match = re.search(pat, sql, re.IGNORECASE)
+            if match:
+                slots[step] = match.group(1)
 
-        # Extract the table name and columns using regex
-        match = re.search(pattern, sql, re.IGNORECASE)
-        if match:
-            return match.group(1)
-        else:
-            return None
-
-import re
-
+        # refine the slots
+        slots['select all'] = True if slots['select'] == '*' else False
+        slots['distinct'] = True if slots['distinct'] else False
+        str = slots['select']
+        slots['select'] = str.split(',') if str else []
+        return slots
+            
 # 示例SQL语句
-sql_query = """
+sql_querys ="""
+select * from table_name;
 SELECT column1, column2 FROM table_name;
 SELECT DISTINCT column1 FROM table_name;
 SELECT column1 FROM table_name WHERE column2 = 'value';
@@ -37,11 +48,14 @@ SELECT column1, COUNT(*) FROM table_name GROUP BY column1 HAVING COUNT(*) > 1;
 SELECT column1 FROM table_name ORDER BY column1 ASC;
 SELECT column1 FROM table_name LIMIT 10;
 SELECT column1 FROM table_name LIMIT 10 OFFSET 20;
-SELECT column1 FROM table_name ORDER BY column1 FETCH FIRST 10 ROWS ONLY;
-"""
+SELECT column1 FROM table_name ORDER BY column1 FETCH FIRST 10 ROWS ONLY;"
+""".split(";")
 
 # Example usage
-extractor = Extractor()
-sql_statement = "SELECT column1, column2 FROM table_name ;"
-result = extractor.extract_info(sql_statement)
-print(result)
+if __name__ == '__main__':
+    te = Extractor()
+    for sql in sql_querys:
+        print(sql)
+        d = te.extract_info(sql)
+        print(d)
+    
