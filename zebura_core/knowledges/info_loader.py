87a3d559a2749ec9b8f95dea1552ccdb243cfb19
harusamei <harusamei@126.com>
2024-04-01 18:41:34 +0800

    ADD: table information and load method

--- zebura_core/knowledges/info_loader.py
+++ zebura_core/knowledges/info_loader.py
@@ -2,31 +2,77 @@
 import yaml
 import os
 import sys
+import json
 if os.getcwd().lower() not in sys.path:
     sys.path.insert(0, os.getcwd().lower())
 from settings import z_config
 from typing import Dict
+# 读metadata of table
 
-# 读yaml文件保存为dict
-def load_sql_re() -> Dict[str,str]:
+class InfoLoader:
+    def __init__(self):
+        self._info = self.load_tableInfo()
+        self.tables = self._info["tables"]
+        
+    def load_tableInfo(self) -> Dict[str,str]:
+        cwd = os.getcwd()
+        path = z_config['Paths','KnowledgePath']
+        name = z_config['Info','table_info']  
+        file = os.path.join(cwd, path, name)
+        print(file)
+        # Load the config file
+        with open(file, 'r',encoding='utf-8-sig') as file:
+            info_dict = json.load(file)
+        return info_dict
+    
+    def get_table(self,tableName) -> Dict[str,str]:
+        tDict = next((table for table in self.tables if table["table"] == tableName), None)
+        return tDict
+    # 表信息或表头信息
+    def get_column(self, tableName, columnName) -> Dict[str,str]:
+        
+        columns = self.get_table(tableName).get("columns")
+        result = next((column for column in columns if column["column_en"] == columnName), None)
+        return result
 
-    cwd = os.getcwd()    
-    path = z_config['Paths', 'KnowledgePath']
-    name = z_config['Patterns', 'sql_re']
-    sp_file = os.path.join(cwd, path, name)
+    def __getitem__(self, key):
+        return self._info.get(key)
+    
+# 读模板
+class PatLoader:
+    def __init__(self):
+        
+        self._sql_pats = self.load_pat()
+    
+    def load_pat(self) -> Dict[str,str]:
+        cwd = os.getcwd()
+        path = z_config['Paths','KnowledgePath']
+        name = z_config['Info','sql_re']  
+        sp_file = os.path.join(cwd, path, name)
 
-    # Load the config file
-    with open(sp_file, 'r',encoding='utf-8') as file:
-        patterns = yaml.safe_load(file)
+        # Load the config file
+        with open(sp_file, 'r',encoding='utf-8') as file:
+            pats = yaml.safe_load(file)
 
-    sql_re = dict()
-    # Save the environment variables in os
-    for key, value in patterns.items():
-       sql_re[key] = str(value)
-    return sql_re
-
+        sql_pats = dict()
+        # Save the environment variables in os
+        for key, value in pats.items():
+            sql_pats[key] = str(value)
+        return sql_pats
+    
+    @property
+    def sql_pats(self):
+        return self._sql_pats
+    
+    def __getitem__(self, key):
+        return self._sql_pats[key]
 
 # Example usage
 if __name__ == '__main__':
-    info_dict= load_sql_re()
-    print(info_dict)
+    # Load the SQL patterns
+    loader = InfoLoader()
+    print(loader['host'])
+    print(loader.get_column('product','brand'))
+    print(loader.get_table('product').get('alias_en'))
+
+    
