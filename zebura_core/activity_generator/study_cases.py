c00bcc0e8ce544d75b293571486f08ee6b15a3bd
harusamei <harusamei@126.com>
2024-04-25 14:03:58 +0800

    FEAT: assemble find by rrf

--- zebura_core/activity_generator/study_cases.py
+++ zebura_core/activity_generator/study_cases.py
@@ -27,7 +27,7 @@ class CaseStudy:
                 if col not in [c['column_en'] for c in self.columns]:
                     raise ValueError(f"Column {col} not found in table {table}")
             
-        
+        # 欧氏距离或manhattan distance, _score 越小越相似，区间是[0, +∞)
         def find_similar_query(self, query, topk=5):
             index = self.gcase_index
             results = self.es.search_word(index, "query", query, topk)
@@ -37,34 +37,58 @@ class CaseStudy:
             index = self.gcase_index
             results = self.es.search_word(index, "sql", sql, topk)
             return results
-        
+        # ES为了保证所有的得分为正，实际使用（1 + 余弦相似度）/ 2，_score [0，1]。得分越接近1，表示两个向量越相似  
         def find_similar_vector(self, query, topk=5):
             index = self.gcase_index
             results = self.es.search_word(index, "qembedding", query, topk)
             return results
         
-        def assemble_find(self, query, sql, topk=5):
-            resp1 = self.find_similar_query(query, topk)
-            resp2 = self.find_similar_sql(sql, topk)
-            resp3 = self.find_similar_vector(query, topk)
+        """
+            Compute the weight using Reciprocal Rank Fusion (RRF) for a list of rank lists.
+            ranks_lists (list of lists): List of rank lists to be fused.
+            Returns: list: Weighted rank list.
+        """
+        @staticmethod
+        def rrf_weighted(ranks_lists):
+            docs={}
+            # k是平滑因子，这里取最大的rank长度
+            k =max(len(rank) for rank in ranks_lists)
+            print("k:",k)
+            # 未出现在rank中的doc score为0
+            for rank in ranks_lists:
+                for i,doc in enumerate(rank):
+                    if not docs.get(doc):
+                        docs[doc] = 1/(i+k)
+                    else:
+                        docs[doc] += 1/(i+k)
             
-            cands = {}
-            all_resp = resp1['hits']['hits']+resp2['hits']['hits']+resp3['hits']['hits']
-            for resp in all_resp:
-                if not cands.get(resp['_id']):
-                    cands[resp['_id']] = resp['_score']
-                else:
-                    cands[resp['_id']] += resp['_score']
-            print(cands)
-            sorted_ids = sorted(cands, key=lambda x: cands[x],reverse=True)
-            results = {}
-            for i, id in enumerate(sorted_ids):
-                results[id] = {'rank':i, 'score':cands[id],'doc':None}
-            
-            for resp in all_resp:
-                if resp['_id'] in results:
-                    results[resp['_id']]['doc'] = resp['_source']
+            sorted_docs = sorted(docs.items(), key=lambda item: item[1], reverse=True)
+            return sorted_docs
 
+        def assemble_find(self, query, sql=None, topk=5) -> list:
+
+            resps = [None]*3
+            resps[0] = self.find_similar_query(query, topk)
+            if sql:
+                resps[1] = self.find_similar_sql(sql, topk)
+            else:
+                resps[1] = {'hits':{'hits':[]}}
+            resps[2] = self.find_similar_vector(query, topk)
+            
+            rank_list=[None]*3
+            for i in range(3):
+                rank_list[i] = [hit['_id'] for hit in resps[i]['hits']['hits']]
+            sorted_ids = self.rrf_weighted(rank_list)
+
+            docs = {}
+            for i in range(3):
+                for hit in resps[i]['hits']['hits']:
+                    docs[hit['_id']] = hit['_source']
+            
+            results = []
+            for i, id in enumerate(sorted_ids):
+                results.append({'doc':docs[id[0]], 'rank':i+1, 'score':id[1]})
+            
             return results
         
 
@@ -75,7 +99,16 @@ if __name__ == '__main__':
     sql = 'select * from product where product_name = "联想小新电脑"'
     results = cs.assemble_find(query, sql)
     
-    for id, result in results.items():
-        print(f"Rank:{result['rank']}, Score:{result['score']}")
+    for result in results:
+        print(f"rank:{result['rank']}, score:{result['score']}")
         print(result['doc'].get('query'))
+
+    # Example usage:
+    rank_list1 = [1,2,3,4,5,6]
+    rank_list2 = [5,4,3,2,1,6]
+    rank_list3 = [1, 2, 6]
+    ranks_lists = [rank_list1, rank_list2, rank_list3]
+
+    weighted_rank_list = cs.rrf_weighted(ranks_lists)
+    print("Weighted Rank List:", weighted_rank_list)
    
