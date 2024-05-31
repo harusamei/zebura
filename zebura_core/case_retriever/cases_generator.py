828cfd979a91eee1739058d30d54b2c109d34624
harusamei <harusamei@126.com>
2024-05-31 18:30:37 +0800

    FEAT:add generate nl questions and sql by LLM

--- zebura_core/case_retriever/cases_generator.py
+++ zebura_core/case_retriever/cases_generator.py
@@ -2,60 +2,37 @@
 import os
 import sys
 sys.path.insert(0, os.getcwd())
-import re
-from settings import z_config
+import asyncio
 import logging
-from knowledges.schema_loader import Loader
-
-
+from zebura_core.LLM.llm_agent import LLMAgent
+from zebura_core.LLM.prompt_loader import prompt_generator
 
 class case_generator:
     def __init__(self):
-        self.sch_loader = Loader(z_config['Training','db_schema'])
+        self.prompter = prompt_generator()
+        self.llm = LLMAgent()
         logging.debug("case_generator init success")
 
     # 生成golden cases
-    def gen_cases(self,table_name:str) -> dict:
-        table_info = self.sch_loader.get_table_info(table_name)
-        if table_info is None:
-            return {   
-                "zh":'',
-                "en":''
-            }
-        
-        desc = table_info.get("desc",'')
-        columns = self.sch_loader.get_all_columns(table_name)
-        columnInfo = [f"{c.get('name_zh','')}: {c.get('desc','')}" for c in columns]
-        columnInfo = "\n".join(columnInfo)
-        db_zh = (
-                    f"表名是{table_info.get('name_zh','')}，用途是{desc}，包含的"
-                    f"列及其含义如下\n{columnInfo}\n"
-                )
-        
-        columnInfo = [f"{c.get('column_name','')}， {c.get('desc','')}" for c in columns]
-        columnInfo = "\n".join(columnInfo)
-        db_en = (
-                    f"The table name is {table_info.get('name_en','')} and its purpose is {desc}. "
-                    f"The columns and their meanings are as follows\n{columnInfo}\n"
-                )
-        return {"zh":db_zh,"en":db_en}
+    async def gen_cases(self,table_name=None) -> dict:
+
+        dbSchema = self.prompter.gen_dbSchema(table_name,style='lite')
+        prompt = self.prompter.gen_default_prompt('db2nl')
+        results = await self.llm.ask_query(dbSchema, prompt)
+
+        return results
     
-    # 生成golden cases
-    def gen_cases(self,table_name:str) -> dict:
-        table_info = self.sch_loader.get_table_info(table_name)
-        if table_info is None:
-            return {   
-                "zh":'',
-                "en":''
-            }
-        
-        desc = table_info.get("desc",'')
-        columns = self.sch_loader.get_all_columns(table_name)
-        columnInfo = [f"{c.get('name_zh','')}: {c.get('desc','')}" for c in columns]
-        columnInfo = "\n".join(columnInfo)
-        db_zh = (
-                    f"表名是{table_info.get('name_zh','')}，用途是{desc}，包含的"
-                    f"列及其含义如下\n{columnInfo}\n"
-                )
-        
-        columnInfo = [f"{c.get('column_name','
+    async def gen_sql(self,table_name=None) -> dict:
+
+        dbSchema = self.prompter.gen_dbSchema(table_name,style='lite')
+        prompt = self.prompter.gen_default_prompt('db2sql')
+        results = await self.llm.ask_query(dbSchema, prompt)
+
+        return results
+    
+# Example usage
+if __name__ == '__main__':   
+    gen = case_generator()
+    table_name = 'products'
+    results = asyncio.run(gen.gen_sql())
+    print(results)
