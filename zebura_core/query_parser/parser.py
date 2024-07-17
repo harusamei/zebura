57b025ce03d5f530af501332d19214d909d68d12
zou@itx <loopscn@qq.com>
2024-07-17 14:40:41 +0800

    增加了session和登录支持

--- zebura_core/query_parser/parser.py
+++ zebura_core/query_parser/parser.py
@@ -9,26 +9,19 @@ import sys
 sys.path.insert(0, os.getcwd())
 from settings import z_config
 import logging
-from zebura_core.query_parser.extractor import Extractor
-from normalizer import Normalizer
-from schema_linker import Sch_linking
+from zebura_core.query_parser.normalizer import Normalizer
 from zebura_core.case_retriever.study_cases import CaseStudy
 from zebura_core.LLM.prompt_loader import prompt_generator
 from zebura_core.constants import D_TOP_GOODCASES as topK
-from constants import D_TOP_GOODCASES as topK
+# from constants import D_TOP_GOODCASES as topK
 from server.msg_maker import make_a_log
 
 class Parser:
 
     def __init__(self):
         self.norm =Normalizer()
-        self.te = Extractor()
         self.gc = CaseStudy()
         self.prompter = prompt_generator()
-
-        cwd = os.getcwd()
-        name = z_config['Training','db_schema']  # 'training\it\products_schema.json'
-        self.sl = Sch_linking(os.path.join(cwd, name))
         logging.debug("Parser init success")
         
 
@@ -36,86 +29,56 @@ class Parser:
     # table_name None, 为多表查询
     # todo, refine()
     async def apply(self, query, table_name=None) -> dict:
-        # 1. Normalize the query to sql format by LLM
-        if table_name is None:
-            print(f"parse.apply()-> all tables, query:{query}")
-        else:
-            print(f"parse.apply()-> table:{table_name}, query:{query}")     
+
+        # if table_name is None:
+        #     print(f"parse.apply()-> all tables, query:{query}")
+        # else:
+        #     print(f"parse.apply()-> table:{table_name}, query:{query}")
 
         resp = make_a_log("parse")
         # few shots from existed good cases
-        gcases = self.find_good_cases(query,topK=topK)
+        # TODO: build amazon_gcases index
+        ##################
+        gcases = []  # self.find_good_cases(query,topK=topK)
+        for case in gcases:
+            if case.get('qemb'):
+                del case['qemb']
+        # 得到 system prompt, fewshots prompt
+        prompt = self.prompter.gen_sql_prompt_fewshots(gcases, table_name)
 
-        #得到 system prompt, fewshots prompt
-        prompt1 = self.prompter.gen_sql_prompt_dial(gcases, table_name,style='lite')
-     
-        logging.info(f"parse.apply()-> generate prompt and call Normalizer for {table_name} and {query}")
+        logging.info(f"parse.apply()-> table_name and query is {table_name} and {query}")
         # query to sql
-        answ = await self.norm.apply(query, prompt1['system'],prompt1['fewshots'])
+        answ = await self.norm.apply(query, prompt['system'], prompt['fewshots'])
+
         resp['msg'] = answ['msg']
         resp['status'] = answ['status']
+        resp['note'] = answ.get('note', '')
+        resp['others']['gcases'] = gcases
         if answ['status'] == "failed":
-            return resp
+            resp['hint'] = answ.get('hint', '')
+        return resp
 
-        sql_1 = resp['msg']
-        # 2. Extract the slots from the query
-        slots1 = self.te.extract(sql_1)
-        # 3. Link the slots to the schema
-        slots2 = self.sl.refine(slots1)
-        # 3. revise the sql query by the slots
-        sql2 = self.gen_sql(slots2)
-        # sql1, slots1 为修正前，sql2, slots2 为修正后
-        if slots1 is None:
-            resp['status'] = "failed"
-
-        merged_dict = {**resp, **{"sql1":sql_1,"sql2":sql2,"slots1":slots1, "slots2":slots2}}
-
-        return merged_dict
-
-    def find_good_cases(self,query,sql=None,topK=topK):
+    def find_good_cases(self, query, sql=None, topK=topK):
         # 从ES中获得候选 topK*1.5
         # {'doc':docs[id[0]], 'rank':i+1, 'score':id[1]}
-        results = self.gc.assemble_find(query,sql,int(topK*1.5))
+        results = self.gc.assemble_find(query, sql, int(topK * 1.5))
         # TODO 用score 过滤？rerank
         new_results = []
         for res in results[:topK]:
             new_results.append(res['doc'])
-        
+
         return new_results
-    
-    # 简单合成，只做了select,form,where
-    def gen_sql(self,slots):
-        if slots is None:
-            return None
-        
-        # "select * from 产品表 where "
-        str_from = 'from '
-        str_from += slots['from']
-        str_select = 'select '
-        str_select+= ",".join(slots["columns"])
-        if slots['distinct']:
-            str_select = str_select.replace("select","select distinct")
-        str_where = 'where '
-        for cond in slots['conditions']:
-            if isinstance(cond,dict):
-                if cond['value'].isdigit():
-                    str_where += f"{cond['column']} {cond['op']} {cond['value']}  "
-                else:
-                    str_where += f"{cond['column']} {cond['op']} '{cond['value']}' "
-            else:
-                str_where += f"{cond} "
-
-        return f"{str_select} {str_from} {str_where}"
-
-
 # Example usage
 if __name__ == '__main__':
     import asyncio
 
-    querys = ['列出类别是电脑的产品名称','哪些产品属于笔记本类别？','列出所有的产品类别']
-    querys =['帮我查一下小新的价格','查一下联想小新电脑的价格','查一下价格大于1000的产品']
-    table_name = 'products'
+    querys = ['目前有哪些电子产品的折扣价格低于500元？','评分在4.5以上的产品有哪些？找出其中最高的不超过5个',
+              '哪些产品的折扣最大？能推荐几款吗？','我想知道这款产品（ID为B09RFB2SJQ）的详细信息，包括名称、价格、折扣和评分。',
+              '我想看看这款产品（ID为B09RFB2SJQ）的用户评论。', '用户RAMKISAN之前写的评论都在哪儿可以找到？',
+              '手动搅拌机这款产品的评分有多少个？平均评分是多少？','电子产品分类下，评分最高的几款产品有哪些？'
+              ]
+    table_name = 'product'
     parser = Parser()
     for query in querys:
         result = asyncio.run(parser.apply(query))
-        print(result)
+        print(f"query:{query}\n{result['msg']}")
--- zebura_core/query_parser/schema_linker.py
+++ zebura_core/query_parser/schema_linker.py
@@ -11,9 +11,18 @@ from zebura_core.utils.compare import similarity
 # todo must match schema
 class Sch_linking:
 
-    def __init__(self,scha_file):
+    # def __init__(self,scha_file):
+    #     self.similarity = similarity()
+    #     self.info_loader = Loader(scha_file)
+    #     logging.info("Schema linking init done")
+    def __init__(self,scha_file, scha_loader=None):
         self.similarity = similarity()
-        self.info_loader = Loader(scha_file)
+        if scha_loader is not None:
+            self.info_loader = scha_loader
+        elif scha_file is not None:
+            self.info_loader = Loader(scha_file)
+        else:
+            raise ValueError("No schema file or schema loader")
         logging.info("Schema linking init done")
 
     def link_table(self, term):
@@ -32,11 +41,34 @@ class Sch_linking:
         like_item = self.get_like_item(term,table_dict)    
         return like_item['name']
             
+    # def link_field(self, term, table_name=None):
+    #     # 如果是数字或者符号，直接返回
+    #     if re.match(r'^[^a-zA-Z\u4e00-\u9fa5]+$', term):
+    #         return table_name, term
+    #
+    #     column_dict = {}
+    #     if table_name is not None:
+    #         table = self.info_loader.get_table_info(table_name)
+    #         tables = [table]
+    #     else:
+    #         tables = self.info_loader.tables
+    #
+    #     for table in tables:
+    #         columns = table['columns']
+    #         table_name = table['table_name']
+    #         for column in columns:
+    #             temStr = f"{column['column_name']},{column.get('name_zh','')},{column.get('alias','')},{column.get('alias_zh','')}"
+    #             temList = re.split(',+\s*|;+\s*',temStr)
+    #             column_dict[f"{table_name}==={column['column_name']}"] = ','.join(temList)
+    #     like_item = self.get_like_item(term,column_dict)
+    #
+    #     table_name, _, field_name = like_item['name'].partition('===')
+    #     return table_name, field_name
+
     def link_field(self, term, table_name=None):
-        # 如果是数字或者符号，直接返回
-        if re.match(r'^[^a-zA-Z\u4e00-\u9fa5]+$', term):
-            return table_name, term
-        
+        if '*' in term:
+            return '*', '*'
+
         column_dict = {}
         if table_name is not None:
             table = self.info_loader.get_table_info(table_name)
@@ -48,36 +80,36 @@ class Sch_linking:
             columns = table['columns']
             table_name = table['table_name']
             for column in columns:
-                temStr = f"{column['column_name']},{column.get('name_zh','')},{column.get('alias','')},{column.get('alias_zh','')}"
-                temList = re.split(',+\s*|;+\s*',temStr) 
+                temStr = f"{column['column_name']},{column.get('name_zh', '')},{column.get('alias', '')},{column.get('alias_zh', '')}"
+                temList = re.split(',+\s*|;+\s*', temStr)
                 column_dict[f"{table_name}==={column['column_name']}"] = ','.join(temList)
-        like_item = self.get_like_item(term,column_dict)
-        
-        table_name, _, field_name = like_item['name'].partition('===')        
+        like_item = self.get_like_item(term, column_dict)
+
+        table_name, _, field_name = like_item['name'].partition('===')
         return table_name, field_name
-    
-    def refine(self,slots1):
-        if slots1 is None or slots1.get('from') is None:
-            return None
-        
-        slots = slots1.copy()
-        tableName = slots['from']
-        st_table = self.link_table(tableName)
-        tableName = slots['from'] = st_table
-        
-        columns = slots['columns']
-        for idx, column in enumerate(columns):
-            st_table, st_col = self.link_field(column, tableName)
-            columns[idx] = st_col
-            
-        # conditions
-        for cond in slots.get('conditions', []):
-            if isinstance(cond, str):
-                continue
-            st_table, st_col = self.link_field(cond['column'], tableName)
-            cond['column'] = st_col
-            
-        return slots
+
+    # def refine(self,slots1):
+    #     if slots1 is None or slots1.get('from') is None:
+    #         return None
+    #
+    #     slots = slots1.copy()
+    #     tableName = slots['from']
+    #     st_table = self.link_table(tableName)
+    #     tableName = slots['from'] = st_table
+    #
+    #     columns = slots['columns']
+    #     for idx, column in enumerate(columns):
+    #         st_table, st_col = self.link_field(column, tableName)
+    #         columns[idx] = st_col
+    #
+    #     # conditions
+    #     for cond in slots.get('conditions', []):
+    #         if isinstance(cond, str):
+    #             continue
+    #         st_table, st_col = self.link_field(cond['column'], tableName)
+    #         cond['column'] = st_col
+    #
+    #     return slots
     
     def get_like_item(self, term, items_dict):
         like_item = {'score':-1,'name':''}
@@ -103,12 +135,10 @@ class Sch_linking:
 # Example usage
 if __name__ == '__main__':
     cwd = os.getcwd()
-    name= 'training/ikura/ikura_meta.json'
+    name= 'training/amazon/amazon_meta.json'
     sch_linking = Sch_linking(os.path.join(cwd, name))
     slots = {'from': 'products', 'columns': ['brand name', 'item price'], 'conditions': [{'column': 'brand', 'op': '=', 'value': '联想'}]}
     slots = {
         'columns': ['COST','market time'],'from': 'sale_info'
         }
     print(slots)
-    result = sch_linking.refine(slots)
-    print(result)
